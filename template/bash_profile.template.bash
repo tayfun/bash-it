4c18040b578b059e91120bc0bcec29b09005d86f
tayfun <tayfun.sen@markafoni.com>
2012-04-19 11:17:08 +0300

    Don't need all those environment variables. I like them simple and clean.

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 # Load RVM, if you are using it
-[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
-
-# Add rvm gems and nginx to the path
-export PATH=$PATH:~/.gem/ruby/1.8/bin:/opt/nginx/sbin
+#[[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
 
 # Path to the bash it configuration
 export BASH_IT=$HOME/.bash_it
@@ -14,26 +11,26 @@ export BASH_IT=$HOME/.bash_it
 export BASH_IT_THEME='bobby'
 
 # Your place for hosting Git repos. I use this for private repos.
-export GIT_HOSTING='git@git.domain.com'
+#export GIT_HOSTING='git@git.domain.com'
 
 # Set my editor and git editor
-export EDITOR="/usr/bin/mate -w"
-export GIT_EDITOR='/usr/bin/mate -w'
+#export EDITOR="/usr/bin/mate -w"
+#export GIT_EDITOR='/usr/bin/mate -w'
 
 # Set the path nginx
-export NGINX_PATH='/opt/nginx'
+#export NGINX_PATH='/opt/nginx'
 
 # Don't check mail when opening terminal.
-unset MAILCHECK
+#unset MAILCHECK
 
 
 # Change this to your console based IRC client of choice.
 
-export IRC_CLIENT='irssi'
+#export IRC_CLIENT='irssi'
 
 # Set this to the command you use for todo.txt-cli
 
-export TODO="t"
+#export TODO="t"
 
 # Set vcprompt executable path for scm advance info in prompt (demula theme)
 # https://github.com/xvzf/vcprompt
